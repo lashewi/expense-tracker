04b03601b94eca70d551beadb36e808a78288f4b
lashewi <swisum@gmail.com>
2020-05-01 20:43:14 +0530

    Add AppReducer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,15 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+
+import {GlobalContext} from '../context/GlobalState'
 
 export const AddTransaction = () => {
 
     const [text, setText] = useState('');
     const [amount, setamount] = useState(0);
+    
+    const {addTransaction} = useContext(GlobalContext) 
 
+    const onSubmit =  e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000), //Improve This
+            text,
+            amount: +amount,
+        }
+
+        addTransaction(newTransaction)
+    }
 
     return (
-        <div>
+        <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                 <label htmlFor="text">Text</label>
                 <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="Enter text..." />
@@ -24,6 +39,6 @@ export const AddTransaction = () => {
                 <button className="btn">Add transaction</button>
             </form>
             
-        </div>
+        </>
     )
 }
